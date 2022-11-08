5b6b0125c328ff84919d28d9c272988ac965b46e
xAsindx <asind@ASIND>
2022-11-08 17:44:22 +0700

    panelworking 1 step

--- InitialValueProblemView.Designer.cs
+++ InitialValueProblemView.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace InitialValueProblem
 {
-    partial class InitialValueProblemView
+    partial class Form1
     {
         /// <summary>
         /// Обязательная переменная конструктора.
@@ -139,8 +139,8 @@
             // 
             // AddSolverNavButton
             // 
-            this.AddSolverNavButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.AddSolverNavButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
             this.AddSolverNavButton.AutoSize = true;
             this.AddSolverNavButton.Location = new System.Drawing.Point(3, 3);
@@ -153,8 +153,8 @@
             // 
             // DeleteSolverNavButton
             // 
-            this.DeleteSolverNavButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.DeleteSolverNavButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
             this.DeleteSolverNavButton.Location = new System.Drawing.Point(3, 45);
             this.DeleteSolverNavButton.Name = "DeleteSolverNavButton";
@@ -166,8 +166,8 @@
             // 
             // SolveNavButton
             // 
-            this.SolveNavButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.SolveNavButton.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
             | System.Windows.Forms.AnchorStyles.Right)));
             this.SolveNavButton.Location = new System.Drawing.Point(3, 87);
             this.SolveNavButton.Name = "SolveNavButton";
@@ -212,9 +212,9 @@
             this.AddSolverPanel.Controls.Add(this.BehaviorComboBox);
             this.AddSolverPanel.Controls.Add(this.TypeComboBox);
             this.AddSolverPanel.Controls.Add(this.AddSolverNameTextBox);
-            this.AddSolverPanel.Location = new System.Drawing.Point(2, 3);
+            this.AddSolverPanel.Location = new System.Drawing.Point(3, 3);
             this.AddSolverPanel.Name = "AddSolverPanel";
-            this.AddSolverPanel.Size = new System.Drawing.Size(439, 251);
+            this.AddSolverPanel.Size = new System.Drawing.Size(594, 251);
             this.AddSolverPanel.TabIndex = 1;
             // 
             // AddSolverButton
